904c815f2ba6ab1669cf6cb19199cf21c80ffcbe
alexerhardt <alex.erhardt.es@gmail.com>
2019-03-08 13:44:17 +0100

    add Row

--- src/components/StickyBoard.js
+++ src/components/StickyBoard.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import StickyNote from './StickyNote';
+import Row from './Row';
+import shortid from 'shortid';
 
 const StickyBoard = (props) => {
+  let toRender;
+  const { notes } = props;
+
+  if (notes.length > 0) {
+    toRender = [];
+    const { numCols, onMemoRemove } = props;
+    const numRows = Math.ceil(notes.length / numCols); 
+
+    for(let i = 0; i < numRows; i+= numCols) {
+      const rowNotes = notes.slice(i, i + numCols);
+      const row = <Row key={shortid.generate()} 
+                       rowIndex={i} 
+                       numCols={numCols} 
+                       notes={rowNotes} 
+                       onMemoRemove={onMemoRemove}
+                    />;
+      toRender = [...toRender, row];
+    } 
+  }
+  else {
+    toRender = <h1>There are no notes. Add one!</h1>;
+  }
+
+  console.log('toRender: ' + toRender);
+
   return (
     <div>
-      {props.notes.map((note, i) => (
-        <StickyNote 
-          text={note} 
-          index={i} 
-          key={i}
-          onMemoRemove={props.onMemoRemove}
-        />
-      ))}
+      {toRender}
     </div>
   )
 }
